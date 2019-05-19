75b61330a21234c6f7f0d93372276a69d76c2e72
deven9852 <deven9852@gmail.com>
2019-05-19 12:45:40 +0530

    Simple animation completed.

---
+++ lib/main.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateMixin{
+
+  AnimationController controller;
+  Animation colorAnimation;
+  Animation sizeAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Defining controller with animation duration of two seconds
+    controller =  AnimationController(vsync: this, duration: Duration(seconds: 2));
+
+    // Defining both color and size animations
+    colorAnimation = ColorTween(begin: Colors.blue, end: Colors.yellow).animate(controller);
+    sizeAnimation = Tween<double>(begin: 100.0, end: 200.0).animate(controller);
+
+    // Rebuilding the screen when animation goes ahead
+    controller.addListener(() {
+      setState(() {});
+    });
+
+    // Repeat the animation after finish
+    controller.repeat(reverse: true);
+
+    //For single time
+    //controller.forward()
+
+    //Reverses the animation instead of starting it again and repeats
+    //controller.repeat(reverse: true);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Animation Demo"),
+      ),
+      body: Center(
+        child: Container(
+          height: sizeAnimation.value,
+          width: sizeAnimation.value,
+          color: colorAnimation.value,
+        ),
+      ),
+    );
+  }
+}
+
